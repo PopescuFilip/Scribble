bb1015ac047839295bca98f72617c3197487f036
marcdaniel <daniel.marc@student.unitbv.ro>
2023-10-28 14:50:08 +0300

    implemented the constructor and getters of the player class

--- Scribble/Scribble/Player.cpp
+++ Scribble/Scribble/Player.cpp
@@ -1,3 +1,37 @@
 import player;
 import "Score.h";
 using Scribble::Player;
+
+Scribble::Player::Player()
+{
+};
+
+Scribble::Player::Player(const std::string& username, const std::string& wordLastGuessed, bool canDraw, const Score& score)
+	:m_username(username), m_wordLastGuessed(wordLastGuessed), m_canDraw(canDraw), m_score(score)
+{
+}
+
+std::string Scribble::Player::GetUsername()
+{
+	return m_username;
+}
+
+std::string Scribble::Player::GetWordLastGuessed()
+{
+	return m_wordLastGuessed;
+}
+
+bool Scribble::Player::GetCanDraw()
+{
+	return m_canDraw;
+}
+
+Score Scribble::Player::GetScore()
+{
+	return m_score;
+}
+
+
+
+
+
--- Scribble/Scribble/player.cppm
+++ Scribble/Scribble/player.cppm
@@ -6,6 +6,13 @@ export module player;
 namespace Scribble {
 	export class Player
 	{
+	public:
+		Player();
+		Player(const std::string& username, const std::string& wordLastGuessed, bool canDraw, const Score& score);
+		std::string GetUsername();
+		std::string GetWordLastGuessed();
+		bool GetCanDraw();
+		Score GetScore();
 	private:
 		std::string m_username;
 		std::string m_wordLastGuessed;
