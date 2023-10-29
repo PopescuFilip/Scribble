d48d88d81ba9e7094c4778f085139015983a704d
Nechita Andreea-Bianca <andreea.nechita@student.unitbv.ro>
2023-10-29 13:54:36 +0200

    implementation of the "GetWordSize" function and the "GetVisibleCharacters"function in word.cpp

--- Scribble/Scribble/Word.cpp
+++ Scribble/Scribble/Word.cpp
@@ -2,3 +2,22 @@ module word;
 
 
 using Scribble::Word;
+
+std::string Word::GetWordSize() const
+{
+    size_t length = word.size();
+    return std::to_string(length);
+}
+
+std::string Word::GetVisibleCharacters() const
+{
+    size_t length = word.size();
+    std::string visibleword = word;
+    for (int i = 0; i < length; ++i) {
+        if (revealedCharacters.find(i) == revealedCharacters.end())
+        {
+            visibleword[i] = '_';
+        }
+    }
+    return visibleword;
+}
