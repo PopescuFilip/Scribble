9a244f9e38471435380b05175b9c950045283072
IoanaMitrofan <ioana.mitrofan@student.unitbv>
2023-11-15 18:01:58 +0200

    Created JoinRoom Qt widget class

---
+++ Scribble/Scribble/JoinRoom.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <QMainWindow>
+#include "ui_JoinRoom.h"
+
+class JoinRoom : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	JoinRoom(QWidget *parent = nullptr);
+	~JoinRoom();
+
+private:
+	Ui::JoinRoomClass ui;
+};
