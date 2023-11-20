0d3ad4338d9fa3472485107f0c328e99e11e55b8
FilipPopescu <filip.popescu@student.unitbv.ro>
2023-11-20 23:52:10 +0200

    created GameStorage class

--- ScribbleServer/ScribbleServer/gameDatabase.h
+++ ScribbleServer/ScribbleServer/gameDatabase.h
@@ -5,6 +5,21 @@ import round;
 import player;
 import score;
 import user;
+#include <sqlite_orm/sqlite_orm.h>
+namespace sql = sqlite_orm;
+
+namespace ScribbleServer
+{
+	class GameStorage
+	{
+	public:
+		bool Initialize();
+
+	private:
+		const std::string kDbFile{ "products.sqlite" };
+
+	};
+}
 
 
 
