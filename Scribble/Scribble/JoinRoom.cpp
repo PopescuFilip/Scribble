11c0ff87eb438abf2a3a8c637b525e836eb49f5d
marcdaniel <daniel.marc@student.unitbv.ro>
2023-11-26 16:42:59 +0200

    Changed the JoinRoom window size

--- Scribble/Scribble/JoinRoom.cpp
+++ Scribble/Scribble/JoinRoom.cpp
@@ -1,9 +1,17 @@
 #include "JoinRoom.h"
+#include <QPixmap>
 
 JoinRoom::JoinRoom(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+	QPixmap background("");
+	background = background.scaled(1280, 1440, Qt::IgnoreAspectRatio);
+	QPalette palette;
+	palette.setBrush(QPalette::Window, background);
+	setPalette(palette);
+	setFixedSize(1280, 720);
+	setGeometry(140, 70, 2560, 1440);
 }
 
 JoinRoom::~JoinRoom()
