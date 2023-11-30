95a82c2b9817ad2db441cec5c5b561b51184a6b5
FilipPopescu <filip.popescu@student.unitbv.ro>
2023-11-30 20:55:42 +0200

    added declaration for base functions in GameDatabase

--- ScribbleServer/ScribbleServer/gameDatabase.h
+++ ScribbleServer/ScribbleServer/gameDatabase.h
@@ -1,24 +1,39 @@
 #pragma once
-#include<iostream>
+//#include <string>
 import word;
 import round;
 import player;
 import score;
 import user;
 
-//#include<sqlite_orm/sqlite_orm.h>
-//namespace sql = sqlite_orm;
+#include<sqlite_orm/sqlite_orm.h>
+namespace sql = sqlite_orm;
 
 namespace ScribbleServer
 {
+	inline auto CreateStorage(const std::string& filename)
+	{
+		return sql::make_storage(
+			filename
+			//tables
+		);
+	}
+	
+	using Storage = decltype(CreateStorage(""));
+
 	class GameStorage
 	{
 	public:
 		bool Initialize();
 
 	private:
+		void PopulateStorage();
+
+	private:
 		const std::string kDbFile{ "products.sqlite" };
 
+	private:
+		Storage m_db = CreateStorage(kDbFile);
 	};
 }
 
