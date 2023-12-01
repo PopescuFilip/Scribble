7953be93b2d636e8e3ddb098b5e6d98f966c4f4a
marcdaniel <daniel.marc@student.unitbv.ro>
2023-12-01 19:23:37 +0200

    Changed the EndScreen window size

--- Scribble/Scribble/EndScreen.cpp
+++ Scribble/Scribble/EndScreen.cpp
@@ -1,9 +1,18 @@
 #include "EndScreen.h"
+#include <QPixmap>
 
 EndScreen::EndScreen(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	QPixmap background("");
+	background = background.scaled(1280, 720, Qt::IgnoreAspectRatio);
+	QPalette palette;
+	palette.setBrush(QPalette::Window, background);
+	setPalette(palette);
+	setFixedSize(1280, 720);
+	setGeometry(140, 70, 2560, 1440);
 }
 
 EndScreen::~EndScreen()
