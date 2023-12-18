caaabe48e83e095e3b4980e76e662a2f428d786f
IoanaMitrofan <ioana.mitrofan@student.unitbv>
2023-12-18 16:54:20 +0200

    Implemented the timer module constructor and the GetDuration() and IsActive() functions

--- ScribbleServer/ScribbleServer/timer.cpp
+++ ScribbleServer/ScribbleServer/timer.cpp
@@ -1,3 +1,19 @@
 import timer;
 
 using ScribbleServer::Timer;
+
+Timer::Timer(const uint16_t& duration):
+	m_duration{duration},
+	m_isActive{false}
+{
+}
+
+uint16_t ScribbleServer::Timer::GetDuration() const
+{
+	return m_duration;
+}
+
+bool ScribbleServer::Timer::IsActive() const
+{
+	return m_isActive;
+}
