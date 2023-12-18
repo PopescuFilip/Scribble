ea1e5e33baaaf2e5a734e2f0ec3ed93cd0a376a7
FilipPopescu <filip.popescu@student.unitbv.ro>
2023-12-18 13:54:06 +0200

    moved game logic from GameRoom to game

--- ScribbleServer/ScribbleServer/game.h
+++ ScribbleServer/ScribbleServer/game.h
@@ -1,5 +1,24 @@
 #pragma once
-class Game
+#include <unordered_map>
+#include "gameDatabase.h"
+import player;
+import word;
+
+namespace ScribbleServer
 {
-};
+	class Game
+	{
+	public:
+
+	public:
+		using Point = std::pair<uint16_t, uint16_t>;
+		using Line = std::pair<Point, Point>;
+
+	private:
+		//GameStorage& m_db;
+		std::unordered_map<int, Player> m_players;
+		std::vector<Line> m_drawing;
+		Word m_currentWord;
+	};
+}
 
--- ScribbleServer/ScribbleServer/gameRoom.h
+++ ScribbleServer/ScribbleServer/gameRoom.h
@@ -1,7 +1,6 @@
 #pragma once
-#include<unordered_set>
-//import round;
-import player;
+#include "game.h"
+import random;
 
 namespace ScribbleServer
 {
@@ -9,13 +8,9 @@ namespace ScribbleServer
 	class GameRoom
 	{
 	public:
-		GameRoom();
-		using Point = std::pair<uint16_t, uint16_t>;
-		using Line = std::pair<Point, Point>;
 
 	private:
-		std::unordered_set<Player> m_players;
-		std::vector<Line> m_drawing;
+		
 	};
 
 }
