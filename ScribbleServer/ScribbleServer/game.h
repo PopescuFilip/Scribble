4065c4cdb9c6fd2272ee80c29dee387bf98a3495
BNechita11 <143636415+BNechita11@users.noreply.github.com>
2023-12-23 13:59:16 +0200

    Added enum class GameState in game.h + declared some new Timer variables

--- ScribbleServer/ScribbleServer/game.h
+++ ScribbleServer/ScribbleServer/game.h
@@ -1,15 +1,24 @@
 #pragma once
 #include <unordered_map>
 #include "gameDatabase.h"
+import <chrono>;
 import player;
 import word;
+import timer;
+
 
 namespace ScribbleServer
 {
 	class Game
 	{
 	public:
-
+		enum class GameState
+		{
+			NotStarted,
+			BetweenRounds,
+			Running,
+			Ended
+		};
 	public:
 		using Point = std::pair<uint16_t, uint16_t>;
 		using Line = std::pair<Point, Point>;
@@ -19,6 +28,10 @@ namespace ScribbleServer
 		std::unordered_map<int, Player> m_players;
 		std::vector<Line> m_drawing;
 		Word m_currentWord;
+
+		Timer roundTimer;
+		Timer startRevealTimer;
+		Timer intervalTimer;
 	};
 }
 
